eaba2937b219d861f2b45279728d036416467ee2
Deivis <deiviswingert@hotmail.com>
2018-03-22 14:49:56 -0300

    recipients: remove recipient card story

--- packages/pilot/stories/components/index.js
+++ packages/pilot/stories/components/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 
-import RecipientCardState from './RecipientCard'
 import RecipientSectionState from './RecipientSection'
 
 storiesOf('Components', module)
