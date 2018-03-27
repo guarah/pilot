82edb303e19e75cd9e5037f1489a6acc0d8df10e
vitorsilvalima <vitor.lima2@fatec.sp.gov.br>
2018-03-27 19:17:26 -0300

    cockpit: add user method to its interface

--- packages/pilot/src/pages/UserSettings/index.js
+++ packages/pilot/src/pages/UserSettings/index.js
@@ -5,12 +5,19 @@ import {
   compose,
 } from 'ramda'
 import { translate } from 'react-i18next'
-
+import { connect } from 'react-redux'
+import cockpit from 'cockpit'
 
 import UserSettings from '../../containers/Settings/User'
 
+const mapStateToProps = ({
+  account: { client },
+}) => ({ client })
+
+
 const enhanced = compose(
   withRouter,
+  connect(mapStateToProps),
   translate()
 )
 
@@ -18,16 +25,35 @@ class UserSettingsPage extends React.Component {
   constructor (props) {
     super(props)
     this.state = null
+    this.client = cockpit(props.client)
+    this.handleRedefinePassword = this.handleRedefinePassword.bind(this)
+  }
+
+  handleRedefinePassword (data) {
+    console.log(data)
+    console.log(this.client)
+    this.client
+      .user.updatePassword(data)
+      .then(response => console.dir(response))
+      .catch(error => console.dir(error))
   }
 
   render () {
+    const {
+      t,
+    } = this.props
+
     return (
-      <UserSettings />
+      <UserSettings
+        handlePasswordFormSubmit={this.handleRedefinePassword}
+        t={t}
+      />
     )
   }
 }
 
-UserSettings.propTypes = {
+UserSettingsPage.propTypes = {
+  client: PropTypes.func.isRequired,
   t: PropTypes.func.isRequired,
 }
 
