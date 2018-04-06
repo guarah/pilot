78108f385c1511c5ba92872e32184b17f526aa69
Leonardo Felipe Nerone <leonardofelipenerone@gmail.com>
2018-04-06 19:21:17 -0300

    balance: add requests

--- packages/cockpit/src/balance/index.js
+++ packages/cockpit/src/balance/index.js
@@ -1,4 +1,17 @@
+import Promise from 'bluebird'
+import { nextBusinessDay } from 'business-moment'
+import moment from 'moment'
+import {
+  __,
+  assoc,
+} from 'ramda'
 import buildResult from './result'
+
+const getBulkAnticipationsLimits = (client, recipientId) =>
+  nextBusinessDay('brazil', moment().toDate())
+    .then(paymentDate => paymentDate.getTime())
+    .then(assoc('payment_date', __, { recipientId, timeframe: 'start' }))
+    .then(client.bulkAnticipations.limits)
 
 const balance = client => (recipientId, {
   count,
@@ -9,7 +22,7 @@ const balance = client => (recipientId, {
   Promise.props({
     recipient: client.recipients.find({ id: recipientId }),
     balance: client.balance.find({ recipientId }),
-    bulk_anticipations_limit: client.bulkAnticipations.limits({ recipientId }),
+    bulk_anticipations_limit: getBulkAnticipationsLimits(client, recipientId),
     bulk_anticipations_pending: client.bulkAnticipations.find({
       recipientId,
       status: 'pending',
